96689ecf98bad3e493ee4ab058be9b0f7ef4c9d7
MoranRon93 <95146638+MoranRon93@users.noreply.github.com>
2025-10-18 17:04:29 -0500

    Update apply_s3.sh

--- terraform/apply_s3.sh
+++ terraform/apply_s3.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # Wrapper script to auto-approve Terraform
+export AWS_ACCESS_KEY_ID="$1"
+export AWS_SECRET_ACCESS_KEY="$2"
 
 cd "$(dirname "$0")"
 
 terraform init
 terraform apply -auto-approve
+
+
